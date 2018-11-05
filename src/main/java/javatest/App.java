f53a9e972e4a94b8b32c5ec439af568a1406b7e2
Kubset <kuba1228334@gmail.com>
2018-11-05 22:09:12 +0100

    small refactor

--- src/main/java/javatest/App.java
+++ src/main/java/javatest/App.java
@@ -2,13 +2,10 @@ package javatest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-/**
- * Hello world!
- *
- */
 @SpringBootApplication
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
