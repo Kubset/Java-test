f916163631bdf16103e4855472d980c08591433a
Kubset <kuba1228334@gmail.com>
2018-11-05 22:10:28 +0100

    add empty TestRepository

---
+++ src/main/java/javatest/repository/ITestRepository.java
@@ -0,0 +1,10 @@
+package javatest.repository;
+
+import javatest.model.Test;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ITestRepository extends JpaRepository<Test, Long> {
+
+}
