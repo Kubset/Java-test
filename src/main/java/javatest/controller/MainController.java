0bffa057a6db411abc581e8df03f5150eb8f3fcc
Kubset <kuba1228334@gmail.com>
2018-11-05 17:27:43 +0100

    implement getMapping for java test and for test with specified id

--- src/main/java/javatest/controller/MainController.java
+++ src/main/java/javatest/controller/MainController.java
@@ -3,6 +3,9 @@ package javatest.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
@@ -11,6 +14,17 @@ public class MainController {
     @GetMapping("/dashboard")
     public String abc() {
         return "dashboard";
+    }
+
+    @GetMapping("java")
+    public String java() {
+        return "java_tests";
+    }
+
+    @GetMapping(path = "java/{id}")
+    @ResponseBody
+    public String aaa(@PathVariable(value = "id") long id) {
+        return String.valueOf(id);
     }
 
 }
