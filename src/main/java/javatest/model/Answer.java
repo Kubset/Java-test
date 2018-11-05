df0bb72e0a9ea7e7ee91f3193553b22307ed869a
Kubset <kuba1228334@gmail.com>
2018-11-05 22:11:07 +0100

    add completly implemented Answer model

---
+++ src/main/java/javatest/model/Answer.java
@@ -0,0 +1,57 @@
+package javatest.model;
+
+
+import javax.persistence.*;
+
+@Entity
+public class Answer {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "serial")
+    private Long id;
+    private String answer;
+    private boolean isCorrect;
+
+    @ManyToOne(targetEntity = Question.class)
+    private Question question;
+
+    public Answer(String answer, boolean isCorrect, Question question) {
+        this.answer = answer;
+        this.isCorrect = isCorrect;
+        this.question = question;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public boolean isCorrect() {
+        return isCorrect;
+    }
+
+    public void setCorrect(boolean correct) {
+        isCorrect = correct;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+}
